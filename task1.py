903bb157b74dbb4959f917d2a71afee54014b47f
af.husnutdinova <af.husnutdinova@tensor.ru>
2023-06-13 15:07:47 +0300

    поправила по замечаниям

--- task1.py
+++ task1.py
@@ -11,9 +11,9 @@ try:
     Contacts = driver.find_element(By.CSS_SELECTOR, '.sbisru-Header__menu-item [href="/contacts"]')
     Contacts.click()
     sleep(2)
-    Tensor = driver.find_element(By.CSS_SELECTOR, '[id="contacts_clients"] [src="/resources/NewDesign/pages/Contacts/images/logo.svg?x_module=23.3206-53"]')
-    Tensor.click()
-    sleep(5)
+    tensor = driver.find_element(By.CSS_SELECTOR, '[id="contacts_clients"] [href="https://tensor.ru/"]')
+    tensor.click()
+    sleep(2)
     driver.switch_to.window(driver.window_handles[1])
     sleep(3)
     sila = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__block4-content .tensor_ru-Index__card-title')
@@ -21,7 +21,6 @@ try:
     assert sila.is_displayed()
     more = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__card-text [href="/about"]')
     more.click()
-    sleep(7)
     assert driver.current_url == tensor_about #роверяем, что перешли на https://tensor.ru/about'
 
 finally:
